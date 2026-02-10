2e1892f0e4dc445912ec3b127b014844f34693a0
lucialndr <lucia.linder@gmail.com>
2026-02-10 12:12:42 +0100

    Mein aktueller lokaler Stand

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,13 +3,16 @@
 </script>
 
 <template>
-  <h1>
-    JD Motorräder
-  </h1>
+  <div class="bg-gradient-to-r from-gray-600 via-blue-600 to-gray-600 p-8 text-white">
+    <h1>
+        JD Motorräder
+   </h1>
+  </div>
 </template>
 
 <style scoped>
 h1 {
   font-size: 3rem;
+  text-align: center;
 }
 </style>
